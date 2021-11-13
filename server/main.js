f240a80e9a5979712295d2532cd06b891c2950bc
Ellery Vernon <tmpks3@gmail.com>
2021-11-13 17:35:54 +0900

    Edit: main.js

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io")
+const ws = require("./ws")
 
 const { createServer } = require("./src/linkspot")
 const { config } = require("./config/config")
@@ -6,6 +7,8 @@ require("./db")
 
 function main(config = {}) {
 	const server = createServer(config)
+	const io = new Server(server)
+	ws(io)
 	server.listen(config.port, () => {
 		console.log(`Running at port ${config.port}`)
 	})
