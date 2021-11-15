b0b1d216a09d4eb97df6741b6473a39367be00dc
Han SeongMin <83461987+ElleryVernon@users.noreply.github.com>
2021-11-15 22:45:23 +0900

    Update mention.controller.js

--- server/src/services/mention/mention.controller.js
+++ server/src/services/mention/mention.controller.js
@@ -4,9 +4,9 @@ const db = require("../../lib/db")
 const { asyncWrapper } = require("../../lib/middlewares/async")
 const { ConflictError, UnauthenticatedError, NotFoundError, BadRequestError } = require("../../lib/errors")
 
-// 맨션 get 
+// 맨션 get 컨트롤러
 const readMention = asyncWrapper(async (req, res) => {
-	const userIds = req.userInfo._id
+	const userIds = req.body
 
 	if (!userIds) {
 		throw new BadRequestError("유효하지 않은 body 데이터입니다.")
