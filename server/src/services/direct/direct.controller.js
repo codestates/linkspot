86fd74faa881f2f3f727b05ddc21924fce49a749
Han SeongMin <83461987+ElleryVernon@users.noreply.github.com>
2021-11-15 20:02:38 +0900

    Update direct.controller.js

--- server/src/services/direct/direct.controller.js
+++ server/src/services/direct/direct.controller.js
@@ -9,41 +9,56 @@ const createDirectMessage = asyncWrapper(async (req, res) => {
 	// req 구성
 	// Authorization: accessToken
 	// body: senderId, receiverID, mentionId, text
-	if (!ObjectId.isValid(req.params.userId)) throw new BadRequestError("userId를 잘 못 입력했습니다.")
-	await db.direct.addMessage({
-		senderId: req.userInfo._id,
-		receiverId: req.params.userId,
-		text: req.body.text,
-	})
+
+	const { userId: receiverId } = req.params
+	const { text } = req.body
+	const { _id: senderId } = req.userInfo
+
+	if (!ObjectId.isValid(receiverId)) {
+		throw new BadRequestError("유효하지 않은 userId 입니다.")
+	}
+
+	await db.direct.addMessage({ senderId, receiverId, text })
 
 	// 있으면 추가 안 할 것.
-	const userInfo = await db.user.findUserInfoByUserId(req.userInfo._id)
-	const receiverInfo = await db.user.findUserInfoByUserId(req.params.userId)
-	if (!receiverInfo) throw new BadRequestError("가입하지 않은 사용자의 userId입니다.")
+	const userInfo = await db.user.findUserInfoByUserId(senderId)
+	const receiverInfo = await db.user.findUserInfoByUserId(receiverId)
+	if (!receiverInfo) {
+		throw new BadRequestError("존재하지 않는 userId 입니다.")
+	}
 
 	// DM을 주고 받는 유저끼리 Direct List에 서로가 없을 경우 등록 있을 경우 등록하지 않습니다.
-	if (!userInfo.directList.includes(receiverInfo._id)) await db.user.updateDirectList(userInfo._id, receiverInfo._id)
-	if (!receiverInfo.directList.includes(userInfo._id)) await db.user.updateDirectList(receiverInfo._id, userInfo._id)
+	if (!userInfo.directList.includes(receiverInfo._id)) {
+		await db.user.updateDirectList(userInfo._id, receiverInfo._id)
+	}
+	if (!receiverInfo.directList.includes(userInfo._id)) {
+		await db.user.updateDirectList(receiverInfo._id, userInfo._id)
+	}
 
-	res.status(StatusCodes.OK).json({ message: "Direct Messsage 저장완료" })
+	res.status(StatusCodes.OK).json({ message: "Direct Messsage 저장 성공" })
 })
 
 const readDirectMessage = asyncWrapper(async (req, res) => {
 	// req 구성
 	// Authorization: accessToken
 	// query: limit, skip
-	if (!ObjectId.isValid(req.params.userId)) throw new BadRequestError("userId를 잘 못 입력했습니다.")
+	const { userId: receiverId } = req.params
+	const { limit, skip } = req.body
+	const { _id: senderId } = req.userInfo
+
+	if (!ObjectId.isValid(req.params.userId)) {
+		throw new BadRequestError("유효하지 않은 userId입니다.")
+	}
 
 	// skip, limit의 기본 설정
-	if (!req.query.limit) req.query.limit = 50
-	if (!req.query.skip) req.query.limit = 0
+	// 기본설정을 제공할 시 페이지네이션에서 skip 과 limit가 둘 다 없는 요청이 여러 번 들어올 경우
+	// 중복된 데이터를 여러번 응답할 수도 있음.
+	if (!limit || !skip) {
+		throw new BadRequestError("유효하지 않은 limit 혹은 skip 입니다.")
+	}
 
-	const directMessages = await db.direct.readMessage({
-		senderId: req.userInfo._id,
-		receiverId: req.params.userId,
-		limit: req.query.limit,
-		skip: req.query.skip,
-	})
+	const directMessages = await db.direct.readMessage({ senderId, receiverId, limit, skip })
+
 	res.status(StatusCodes.OK).json({ data: directMessages })
 })
 
@@ -51,16 +66,33 @@ const updateDirectMessage = asyncWrapper(async (req, res) => {
 	// req 구성
 	// Authorization: accessToken
 	// body: messageId, mentionId, text
-	if (!ObjectId.isValid(req.params.userId)) throw new BadRequestError("userId를 잘 못 입력했습니다.")
-	if (!ObjectId.isValid(req.params.messageId)) throw new BadRequestError("messageId를 잘 못 입력했습니다.")
-	const message = await db.direct.findDMbyMessageId(req.params.messageId)
-	if (!message) throw new BadRequestError("존재하지 않는 DM입니다.")
-	if (!req.body.text) throw new BadRequestError("text를 입력하세요.")
-	if (String(message.senderId) !== String(req.userInfo._id)) throw new BadRequestError("권한이 없습니다.")
-	await db.direct.updateMessage({
-		messageId: req.params.messageId,
-		text: req.body.text,
-	})
+
+	const { text } = req.body
+	const { userId, messageId } = req.params
+	const { userInfo } = req
+
+	if (!text) {
+		throw new BadRequestError("유효하지 않은 body 데이터입니다.")
+	}
+
+	if (!ObjectId.isValid(userId)) {
+		throw new BadRequestError("유효하지 않은 userId 입니다.")
+	}
+
+	if (!ObjectId.isValid(messageId)) {
+		throw new BadRequestError("유효하지 않은 messageId 입니다.")
+	}
+
+	const message = await db.direct.findDMbyMessageId(messageId)
+	if (!message) {
+		throw new BadRequestError("존재하지 않는 messageId 입니다.")
+	}
+
+	if (String(message.senderId) !== String(userInfo._id)) {
+		throw new UnauthenticatedError("권한이 필요합니다.")
+	}
+
+	await db.direct.updateMessage({ messageId, text })
 
 	res.status(StatusCodes.OK).json({ message: "Direct Message 수정완료" })
 })
@@ -69,22 +101,46 @@ const deleteDirectMessage = asyncWrapper(async (req, res) => {
 	// req 구성
 	// Authorization: accessToken
 	// body: messageId
-	if (!ObjectId.isValid(req.params.userId)) throw new BadRequestError("userId를 잘 못 입력했습니다.")
-	if (!ObjectId.isValid(req.params.messageId)) throw new BadRequestError("messageId를 잘 못 입력했습니다.")
-	const message = await db.direct.findDMbyMessageId(req.params.messageId)
-	if (!message) throw new BadRequestError("존재하지 않는 DM입니다.")
-	if (String(message.senderId) !== String(req.userInfo._id)) throw new BadRequestError("권한이 없습니다.")
-	await db.direct.deletMessage(req.params.messageId)
-	res.status(StatusCodes.OK).json({ message: "Direct Message 삭제완료" })
+
+	const { userId, messageId } = req.params
+	const { userInfo } = req
+
+	if (!ObjectId.isValid(userId)) {
+		throw new BadRequestError("유효하지 않은 userId 입니다.")
+	}
+
+	if (!ObjectId.isValid(messageId)) {
+		throw new BadRequestError("유효하지 않은 messageId 입니다.")
+	}
+
+	const message = await db.direct.findDMbyMessageId(messageId)
+	if (!message) {
+		throw new BadRequestError("존재하지 않는 messageId 입니다.")
+	}
+
+	if (String(message.senderId) !== String(userInfo._id)) {
+		throw new UnauthenticatedError("권한이 필요합니다.")
+	}
+
+	await db.direct.deletMessage(messageId)
+	res.status(StatusCodes.OK).json({ message: "Direct Message 삭제 성공" })
 })
 
 const deleteDirectList = asyncWrapper(async (req, res) => {
 	// req 구성
 	// Authorization: accessToken
 	// body: senderId, receiverId
-	if (!ObjectId.isValid(req.params.userId)) throw new BadRequestError("userId를 잘 못 입력했습니다.")
-	await db.direct.deletDirectList(req.userInfo._id, req.params.userId)
-	res.status(StatusCodes.OK).json({ message: "Direct Connection List 삭제완료" })
+
+	const { userId } = req.params
+	const { userInfo } = req
+
+	if (!ObjectId.isValid(userId)) {
+		throw new BadRequestError("유효하지 않은 userId 입니다.")
+	}
+
+	await db.direct.deletDirectList(userInfo._id, userId)
+
+	res.status(StatusCodes.NO_CONTENT).json()
 })
 
 module.exports = {
