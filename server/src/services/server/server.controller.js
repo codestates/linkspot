73604a2de6a9fd7c704543499b32b78342c01e5c
Han SeongMin <83461987+ElleryVernon@users.noreply.github.com>
2021-11-15 22:38:25 +0900

    Update server.controller.js

--- server/src/services/server/server.controller.js
+++ server/src/services/server/server.controller.js
@@ -23,14 +23,7 @@ const deleteServer = asyncWrapper(async (req, res) => {
 	// req 구성
 	// cookies: authorization[로그인 인증용 JWT 토큰]
 	// params: serverId[삭제 서버 Id]
-
-	// db에 존재하는 서버인지 확인해야함
-	// 존재하지 않는 서버일 경우
-	// 응답코드(400)와 { message: "존재하지 않는 서버입니다." } 응답 해야함
-
-	// 유저가 서버의 어드민인지 확인해야함
-	// 어드민이 아닐경우
-	// 응답코드(401)와 { message: "서버의 관리자 권한이 필요합니다." } 응답 해야함
+	
 	const { userInfo } = req
 	const { serverId } = req.params
 
@@ -61,14 +54,7 @@ const leaveServer = asyncWrapper(async (req, res) => {
 	// req 구성
 	// cookies: authorization[로그인 인증용 JWT 토큰]
 	// params: serverId[탈퇴 서버 Id]
-
-	// db에 존재하는 서버인지 확인해야함
-	// 존재하지 않는 서버일 경우
-	// 응답코드(400)와 { message: "존재하지 않는 서버입니다." } 응답 해야함
-
-	// 유저가 서버에 참가되어 있는지 확인해야함
-	// 참가되어 있지 않은 경우
-	// 응답코드(400)와 { message: "가입되어 있지 않은 서버입니다." } 응답 해야함
+	
 	await db.server.leaveServer(
 		req.userInfo._id,
 		req.params.serverId
